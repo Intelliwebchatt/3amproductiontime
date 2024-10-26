35a1f2578c13304252e230626b79196cec1898b4
Intelliwebchatt <aslockhart10@gmail.com>
2024-10-26 14:23:12 -0500

    Create Footer.tsx

---
+++ src/components/Footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className="glass-card m-4 p-4 text-center">
+      <p>&copy; {new Date().getFullYear()} YourBrand. All rights reserved.</p>
+    </footer>
+  )
+}
